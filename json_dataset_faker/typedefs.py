8cb9d865461e416c6702113b77880f3126d7a04a
Alexandre Gomes <alexandre.gomes@laura-br.com>
2023-02-06 21:49:09 -0300

    first release

---
+++ json_dataset_faker/typedefs.py
@@ -0,0 +1,6 @@
+from typing import Any, Dict, List, Text, TypeAlias
+
+JsonDict: TypeAlias = Dict[Text, Any]
+JsonList: TypeAlias = List[Dict[Text, Any]]
+String: TypeAlias = Text
+StrList: TypeAlias = List[Text]
